63f08d0c403e48e1ec7c53227c3b3e199654e3f2
KeyboardKobold <bodi@bodi.science>
2020-06-14 01:16:13 +0200

    added general panel layout

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
   <div id="app">
     <div id="content">
-      <Navigation class="navigation"></Navigation>
+      <Navigation/>
+      <div id="panels">
+        <Skills/>
+        <Projects/>
+        <!-- Demos-->
+        <div/>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Navigation from "@/components/Navigation";
+import Skills from "@/components/Skills";
+import Projects from "@/components/Projects";
 
 export default {
   name: 'App',
   components: {
+    Projects,
+    Skills,
     Navigation,
   }
 }
@@ -27,8 +37,21 @@ export default {
   background-color: #272727;
 }
 
-.navigation {
+#panels {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-evenly;
 
+  margin: 0 20% 0 20%;
 }
+
+#panels *{
+  size: auto;
+  border: 1px solid;
+  border-color: black;
+  flex-grow: 1;
+  flex-basis: 0;
+}
+
 
 </style>
