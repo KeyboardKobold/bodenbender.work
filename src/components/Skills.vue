9d1a28525e19a17a1906aa20cb8bae489ce37aee
KeyboardKobold <christian@bodenbender.work>
2021-08-23 22:03:29 +0200

    expanded and updated skill set

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -17,63 +17,94 @@
 
           <div class="skills-table">
             <div class="skill-list">
-              <h3>Programming Languages</h3>
+              <h3>Languages</h3>
               <ul>
-                <li>Java, JEE</li>
+                <li>Java</li>
                 <li>Kotlin</li>
                 <li>Python</li>
-                <li>C</li>
-                <li>Basic</li>
-                <li>SQL</li>
                 <li>Javascript</li>
+                <li>Python</li>
+                <li>SQL</li>
                 <li>HTML/CSS</li>
               </ul>
             </div>
             <div class="skill-list">
-              <h3>Frameworks and Tooling</h3>
+              <h3>Development environments</h3>
+              <ul>
+                <li>IntelliJ</li>
+                <li>Eclipse</li>
+                <li>PyCharm</li>
+                <li>Visual Studio Code</li>
+                <li>Vim</li>
+                <li>Notepad++</li>
+                <li>WebStorm</li>
+              </ul>
+            </div>
+            <div class="skill-list">
+              <h3>VCS</h3>
+              <ul>
+                <li>Git</li>
+                <li>SVN</li>
+                <li>Perforce</li>
+              </ul>
+            </div>
+            <div class="skill-list">
+              <h3>Tooling</h3>
               <ul>
                 <li>Docker</li>
                 <li>Kubernetes</li>
-                <li>GitLab CI/CD</li>
                 <li>Maven</li>
                 <li>Gradle</li>
-                <li>Git</li>
-                <li>Perforce</li>
-                <li>Vertx</li>
-                <li>MongoDB</li>
-                <li>MySQL, PostgreSQL</li>
+                <li>Gitlab CI</li>
+                <li>Jenkins</li>
+                <li>WSL</li>
+                <li>WSL2</li>
+              </ul>
+            </div>
+            <div class="skill-list">
+              <h3>Others</h3>
+              <ul>
+                <li>NoSQL</li>
+                <li>JUnit</li>
+                <li>VertX</li>
+                <li>Testcontainers</li>
+                <li>WebDriver</li>
+                <li>JSON</li>
+                <li>HTML</li>
+                <li>XML</li>
+                <li>Postman</li>
                 <li>VueJS</li>
                 <li>AWS</li>
+                <li>Azure</li>
               </ul>
             </div>
           </div>
         </div>
 
         <div class="work toplevel">
-          <h2>Employment</h2>
-          <p>Graduated <a href="https://www.thm.de/">TH Mittelhessen University of Applied Sciences</a> in March of 2019, attaining a Bachelor of Science in Computer Science.</p>
+          <h2>Employment History</h2>
           <ul class="no-bullets">
             <li>
               <p>
-                04/19 to today - Software Developer, Tallence AG<br>
+                04/19 to today - Software Developer, <a href="https://tallence.com/">Tallence AG</a><br>
                 Development of software in customer projects and in products
               </p>
             </li>
             <li>
               <p>
-                08/17 to 03/19 - Student Trainee Developer, IoT Analytics Real-time Insights, Software AG<br>
+                08/17 to 03/19 - Student Trainee Developer, IoT Analytics Real-time Insights, <a href="https://www.softwareag.com/">Software AG</a><br>
                 Development of analytics software in the R&D IoT Analytics department
               </p>
             </li>
             <li>
               <p>
-                10/15 to 08/17 - Student Developer, AG Softwaretechnik, Philipps-University-Marburg<br>
+                10/15 to 08/17 - Student Developer, AG Softwaretechnik, <a href="https://www.uni-marburg.de/en">Philipps-University-Marburg</a><br>
                 Development and maintenance of the Eclipse plugin 'Henshin'
               </p>
             </li>
             <li>
               <p>
-                04/12 to 07/13 - Student Developer, weclapp SE<br>
+                04/12 to 07/13 - Student Developer, <a href="https://www.weclapp.com/en/">weclapp SE</a><br>
                 Development of automated Test-Framework and expansion of several software instances
               </p>
             </li>
